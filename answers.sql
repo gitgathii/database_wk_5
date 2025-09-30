f389c743f33eb6856b48a6ff77c3f1549528a7fb
owen gathii <owengathii98@gmail.com>
2025-09-30 12:08:58 +0300

    Update answers.sql

---
+++ answers.sql
@@ -0,0 +1,11 @@
+--question 1
+DROP INDEX idxphone on customer; 
+
+--question 2
+CREATE USER 'BOB'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+--question 3
+GRANT INSERT ON Databasedb to 'bob'@'localhost' WITH GRANT OPTION;
+
+--question 4
+ALTER USER 'bob'@'localhost' identified by 'P$55!23'
